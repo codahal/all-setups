d28dc0f8b2a0640db099170ff680b94ea39a0c6e
sri harsha <109609898+codahal@users.noreply.github.com>
2025-11-21 09:51:51 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -7,6 +7,8 @@ sudo yum install jenkins git -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
+
+add it for t3 micro additional setup
 sudo mkdir -p /var/tmp_disk
 sudo chmod 1777 /var/tmp_disk
 sudo mount --bind /var/tmp_disk /tmp
